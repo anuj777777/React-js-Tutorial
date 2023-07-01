78f4d67f3dddcd102ed54f8edf4c55d4f58e3c8a
Anuj kunar <ayushman45679@gmail.com>
2023-07-01 07:19:26 +0530

    Component And Props

---
+++ src/Components/Video.jsx
@@ -0,0 +1,9 @@
+function Video() {
+  return (
+    <div>
+      <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="abc" />
+      <div>React JS Tutorial </div>
+      </div>
+  ); 
+}
+export default Video;
